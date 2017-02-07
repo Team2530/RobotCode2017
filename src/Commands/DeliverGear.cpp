f504f6e6787595f87931b6a875ead462aef38807
Tanner <7551401@isd535.org>
2017-02-06 19:20:36 -0600

    DeliverGear.cpp Tracker Portion Updated
    Updated the Tracker portion of DeliverGear.cpp.

--- src/Commands/DeliverGear.cpp
+++ src/Commands/DeliverGear.cpp
@@ -69,18 +69,27 @@ void Tracker::InitDefaultCommand() {
 	// Set the default command for a subsystem here.
 	// SetDefaultCommand(new MySpecialCommand());
 	SetDefaultCommand(new GetFieldPosition());
+	frontEncoder = new Encoder (ControllerConstants::DIOPort::kDIO2, ControllerConstants::DIOPort::kDIO3, true, CounterBase:: k4X); //check ports
+	sideEncoder = new Encoder (ControllerConstants::DIOPort::kDIO0, ControllerConstants::DIOPort::kDIO1, true, CounterBase:: k4X);
+	frontEncoder->SetDistancePerPulse(kFrontDistancePerPulse); //check circumference/(pulses per revolution)
+	sideEncoder->SetDistancePerPulse(kSideDistancePerPulse);
+
 }
 void Tracker::GetPosition(){
-	double xValue;
-	double yValue;
-	Tracker->UpdateDisplacement(float GetRawAccelX, float GetRawAccelY, int 50, bool true);
-}
-ExampleCommand::ExampleCommand() {
+	double xValue;//can be set depending where are on map
+	double yValue;//^^
 
-	// Use Requires() here to declare subsystem dependencies
-	// eg. Requires(Robot::chassis.get());
-
+	double distanceX = sideEncoder->GetDistance();
+	double distanceY = frontEncoder->GetDistance();
+	double angle = ahrs->GetAngle();
 }
+
+
+// Put methods for controlling this subsystem
+// here. Call these from Commands.
+
+// Use Requires() here to declare subsystem dependencies
+// eg. Requires(Robot::chassis.get());
 
 // Called just before this Command runs the first time
 
