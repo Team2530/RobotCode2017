b76b2b59ce35a530b21c413ddfb726f6f78e993c
Spero Galaty <galatyspero@gmail.com>
2017-02-13 19:03:49 -0600

    Added CommandGroups
    Started work on command groups have put in the delivergears command group.

---
+++ src/CommandGroups/DeliverGears.h
@@ -0,0 +1,24 @@
+/*
+ * DeliverGears.h
+ *
+ *  Created on: Feb 13, 2017
+ *      Author: Admin
+ */
+
+#ifndef SRC_COMMANDGROUPS_DELIVERGEARS_H_
+#define SRC_COMMANDGROUPS_DELIVERGEARS_H_
+
+#include "WPIlib"
+
+
+//#include "DeliverGears1"
+//#include "DeliverGears2"
+//#include "DeliverGears3"
+
+class CommandGroup : public DeliverGears {
+
+};
+
+
+
+#endif /* SRC_COMMANDGROUPS_DELIVERGEARS_H_ */
