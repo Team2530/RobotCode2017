2c463c6e3accfa80e6308c09e7871fda510e3c87
Nicholas Scheel <nasalmusician@gmail.com>
2017-02-13 18:58:25 -0600

    Add Vision subsystem
    Keeps track of the NetworkTable results and initializes the CameraServer.

--- src/Robot.h
+++ src/Robot.h
@@ -31,6 +31,7 @@
 #include "Subsystems/Intake.h"
 #include "Subsystems/Dumper.h"
 #include "Subsystems/Tracker.h"
+#include "Subsystems/Vision.h"
 
 
 
@@ -42,6 +43,7 @@ public:
 	static std::unique_ptr<OI> oi;
 	static std::shared_ptr<Intake> intake;
 	static std::shared_ptr<Tracker> tracker;
+	static std::shared_ptr<Vision> vision;
 
 	void RobotInit() override;
 
