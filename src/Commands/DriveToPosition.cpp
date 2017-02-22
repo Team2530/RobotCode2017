8554ec04e32bbcceac5865f873217fd6bec69a5e
Tanner <7551401@isd535.org>
2017-02-21 18:25:45 -0600

    Little Addition to DriveToPosition

--- src/Commands/DriveToPosition.cpp
+++ src/Commands/DriveToPosition.cpp
@@ -32,11 +32,11 @@ bool DriveToPosition::IsFinished() {
 
 // Called once after isFinished returns true
 void DriveToPosition::End() {
-
+	Robot::drivetrain->Stop();
 }
 
 // Called when another command which requires one or more of the same
 // subsystems is scheduled to run
 void DriveToPosition::Interrupted() {
-
+	Robot::drivetrain->Stop();
 }
