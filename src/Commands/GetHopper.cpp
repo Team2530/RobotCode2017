3d33b1965fbfe286563771e16ceacd15c8efbc5e
Nicholas Scheel <nasalmusician@gmail.com>
2017-02-25 14:42:40 -0600

    Added todo

--- src/Commands/GetHopper.cpp
+++ src/Commands/GetHopper.cpp
@@ -1,16 +1,16 @@
 #include "GetHopper.h"
 #include "Robot.h"
-#include "DriveToPosition.h"
 #include "OrientRobot.h"
+#include "DriveToPosition.h"
 #include "IntakeOn.h"
 GetHopper::GetHopper() {
 	double *x = &Robot::hopperX;
 	double *y = &Robot::hopperY;
 	double *r = &Robot::hopperR;
-	AddSequential(new DriveToPosition(x, y));
 	AddSequential(new OrientRobot(r));
+	AddSequential(new DriveToPosition(x, y));
 	AddSequential(new IntakeOn);
-
+	//TODO::add ability for robot to strafe back and forth to collect balls during Autonomous
 
 
 
