bfb4665a19bd6d71ee824e3aa7bdde8357bd8991
SperoGalaty <tim.torzewski@charter.net>
2017-03-02 19:26:34 -0600

    Added Crossbase Field Postion

---
+++ src/FieldPositions/CrossBaseLineFieldPostion.cpp
@@ -0,0 +1,18 @@
+/*
+ * CrossBaseLineFieldPostion.cpp
+ *
+ *  Created on: Mar 2, 2017
+ *      Author: Admin
+ */
+
+#include <FieldPositions/CrossBaseLineFieldPostion.h>
+
+CrossBaseLineFieldPostion::CrossBaseLineFieldPostion() {
+	// TODO Auto-generated constructor stub
+
+}
+
+CrossBaseLineFieldPostion::~CrossBaseLineFieldPostion() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/FieldPositions/CrossBaseLineFieldPostion.h
@@ -0,0 +1,19 @@
+/*
+ * CrossBaseLineFieldPostion.h
+ *
+ *  Created on: Mar 2, 2017
+ *      Author: Admin
+ */
+
+#ifndef SRC_FIELDPOSITIONS_CROSSBASELINEFIELDPOSTION_H_
+#define SRC_FIELDPOSITIONS_CROSSBASELINEFIELDPOSTION_H_
+
+#include <FieldPositions/FieldPosition.h>
+
+class CrossBaseLineFieldPostion: public FieldPosition {
+public:
+	CrossBaseLineFieldPostion();
+	virtual ~CrossBaseLineFieldPostion();
+};
+
+#endif /* SRC_FIELDPOSITIONS_CROSSBASELINEFIELDPOSTION_H_ */
