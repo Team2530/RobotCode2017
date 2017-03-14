2a31b02cef911686202b1b04be7a67a478d5cb50
William Sit <wsdragonoid62@gmail.com>
2017-03-14 18:30:09 -0500

    Started DeliverFuelAndDumpHopper

---
+++ src/CommandGroups/DeliverGearAndDumpHopper.h
@@ -0,0 +1,11 @@
+#ifndef DeliverGearAndDumpHopper_H
+#define DeliverGearAndDumpHopper_H
+
+#include <Commands/CommandGroup.h>
+
+class DeliverGearAndDumpHopper : public CommandGroup {
+public:
+	DeliverGearAndDumpHopper();
+};
+
+#endif  // DeliverGearAndDumpHopper_H
