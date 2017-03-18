2ffcf769ffbed61d739cda5e1cb5cc151741c5cd
Spero Galaty <galatyspero@gmail.com>
2017-03-18 12:04:43 -0500

    Added Fuel&Gear
    Added a command group to run deliver gear and dump a hopper.

---
+++ src/Commands/DeliverGearDumpHopper.h
@@ -0,0 +1,12 @@
+#ifndef DeliverGearDumpHopper_H
+#define DeliverGearDumpHopper_H
+
+
+#include <Commands/CommandGroup.h>
+
+class DeliverGearDumpHopper : public CommandGroup {
+public:
+	DeliverGearDumpHopper();
+};
+
+#endif  // DeliverGearDumpHopper_H
